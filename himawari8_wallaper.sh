829992c11482e2a12bd3844784803222d8a47457
Ihor Yatsenko <Ihor_Yatsenko@epam.com>
2016-02-09 00:54:38 +0200

    Refactored script. Added comments

--- himawari8_wallaper.sh
+++ himawari8_wallaper.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 width=550
 level="4d" #Level can be 4d, 8d, 16d, 20d 
 numblocks=4 #this apparently corresponds directly with the level, keep this exactly the same as level without the 'd'
@@ -7,31 +6,27 @@ numblocks=4 #this apparently corresponds directly with the level, keep this exac
 time_stamp=$(date +'%Y%m%d%H%M%S')
 short_time_stamp=$(date +'%H%M%S')
 
-
+#get lastest.json with information about last photo
 latest_info_url="http://himawari8-dl.nict.go.jp/himawari8/img/D531106/latest.json"
+#extract from json date and time
+date_time_photo_str=`curl $latest_info_url |grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2} [[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}' `
+#extract from json time
+time_photo_str=`echo $date_time_photo_str |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}' `
 
-date_photo=`curl $latest_info_url |grep -Eo '[[:digit:]]{4}-[[:digit:]]{2}-[[:digit:]]{2} [[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}' `
-time_photo=`echo $date_photo |grep -Eo '[[:digit:]]{2}:[[:digit:]]{2}:[[:digit:]]{2}' `
+#debug info
+echo "Time photo: $time_photo_str"
+echo "The last photo was took: $date_time_photo_str"
 
-echo "time photo: $time_photo"
+#convert date_time_photo_str to url format
+#example: from 2016-02-09 22:40:00 to 2016/02/09/224000 
+date_time_url=`echo $date_time_photo_str | sed 's/[- ]/\//g' | sed 's/[:]//g'`
+#convert date_time_photo_str to url format
+#example: from 22:40:00 224000 
+time_url=`echo $time_photo_str | sed 's/[:]//g'`
 
-echo "The last photo was took: $date_photo"
-
-date_time_url=`echo $date_photo | sed 's/[- ]/\//g' | sed 's/[:]//g'`
-
-echo "date_time_url: $date_time_url"
-
-
-time_url=`echo $time_photo | sed 's/[:]//g'`
-echo "time_url $time_url"
-
-
-
-#time=224000
-
-
-
+#url with parameters to photo 
 base_url="http://himawari8-dl.nict.go.jp/himawari8/img/D531106/$level/$width/$date_time_url"
+#temporary folder for save parts of photo
 tmp_folder="$HOME/tmp$time_stamp"
 
 echo $tmp_folder
@@ -40,8 +35,6 @@ file_ext=".png"
 outfile="result_$time_stamp$file_ext"
 
 echo "Outfile: $outfile"
-
-
 
 current_path=$(pwd)
 
@@ -55,15 +48,13 @@ wallaper_path="$HOME/Pictures/Himawari8/"
 mkdir $tmp_folder
 cd $tmp_folder
 
-count=0		wget -i -P -q $real_url
+count=0
 for ((i=0; i < $numblocks; i++))
 do
 	for ((j=0; j < $numblocks; j++))
 	do
 	   	real_url=$base_url"_"$i"_"$j$file_ext
-
-	   	echo "Real url: $download_file"
-
+	   	echo "Real url: $real_url"
   		wget -i -P -q $real_url
    		echo "Download to: $tmp_folder"
    		file_name=$short_time_stamp"_"$j"_"$i$file_ext
@@ -99,6 +90,7 @@ echo "cureent path $current_path"
 cd $current_path
 rm -rf $tmp_folder
 
+#Number of worspaces
 workspace_count=$(xfconf-query -v -c xfwm4 -p /general/workspace_count)
 connected_monitor=$(xrandr -q | awk '/ connected/{print $1}')
 xfce_desktop_prop_prefix=/backdrop/screen0/monitor$connected_monitor
@@ -106,11 +98,12 @@ xfce_desktop_prop_prefix=/backdrop/screen0/monitor$connected_monitor
 echo "Wallaper path: $wallaper_path"
 echo "Outfile: $outfile"
 
+#Set wallaper for all worspaces(only for XFCE)
 for ((i=1; i <= $workspace_count; i++))
 do
    xfconf-query -c xfce4-desktop -p $xfce_desktop_prop_prefix/workspace$i/last-image -s $wallaper_path$outfile
    xfconf-query -c xfce4-desktop -p $xfce_desktop_prop_prefix/workspace$i/image-style -s 5
-   echo "set $wallaper_path$outfile for workspace $i"
+   echo "Set $wallaper_path$outfile for workspace $i"
 done
 
 
